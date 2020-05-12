c10b45caa53a8a0627c1f9e83cd528b882824bf9
LXie-Saintex <admin@saintex.co.uk>
2020-05-12 08:33:45 +0100

    addded tests for decoders

--- tests/Example.elm
+++
@@ -1,9 +0,0 @@
-module Example exposing (..)
-
-import Expect exposing (Expectation)
-import Fuzz exposing (Fuzzer, int, list, string)
-import Test exposing (..)
-
-
-describe "todos"
-    [ todo "decoder" ]
---
+++ tests/MainTest.elm
@@ -0,0 +1,68 @@
+module MainTest exposing (..)
+
+import Expect exposing (equal)
+import Json.Decode
+import Main exposing (Response, respDecoder)
+import Test exposing (..)
+
+
+suite : Test
+suite =
+    describe "The decoders"
+        [ describe "Definition decoder"
+            [ test "find according fields" <|
+                \_ ->
+                    let
+                        input =
+                            """
+                            [{ "meta": 
+                                { "app-shortdef" : 
+                                    { "hw" : "apple", "fl" : "noun" }
+                                , "offensive" : false
+                                }
+                            , "shortdef" : ["a kind of fruit"]
+                            }]
+                        
+                            """
+
+                        expected =
+                            Result.Ok
+                                (Main.Def
+                                    { word = "apple"
+                                    , fl = "noun"
+                                    , def = "a kind of fruit"
+                                    , isOffensive = False
+                                    }
+                                )
+
+                        actual =
+                            Json.Decode.decodeString respDecoder input
+                    in
+                    Expect.equal expected actual
+            ]
+        ,
+        describe "Alternatives decoder"
+            [ test "decode correctly" <|
+                \_ ->
+                    let
+                        input =
+                            """
+                            ["boy", "coy", "ploy", "soya"]
+                            """
+
+                        expected =
+                            Result.Ok
+                                (Main.Alt
+                                    { first = "boy"
+                                    , second = "coy"
+                                    , third = "ploy"
+                                    , fourth = "soya"
+                                    }
+                                )
+
+                        actual =
+                            Json.Decode.decodeString respDecoder input
+                    in
+                    Expect.equal expected actual
+            ]
+        ]
